8fae5af73025577d94e80fa9318c00ec709926f4
Maladesbola <echiptacreator@gmail.com>
2026-01-10 19:31:40 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,25 +1,7 @@
-# db/database.py
-import sqlite3
-from pathlib import Path
+import os
+import psycopg2
 
-DB_PATH = Path(__file__).parent / "database.db"
+DATABASE_URL = os.getenv("postgresql://postgres:FMGmkhnZAhcDMkFfLYVBwsGBcrLGEffF@postgres.railway.internal:5432/railway")
 
 def get_conn():
-    return sqlite3.connect(DB_PATH, check_same_thread=False)
-
-def init_db():
-    conn = get_conn()
-    cur = conn.cursor()
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS subscriptions (
-        user_id INTEGER PRIMARY KEY,
-        username TEXT,
-        status TEXT,
-        paid_until TEXT,
-        created_at INTEGER
-    )
-    """)
-
-    conn.commit()
-    conn.close()
+    return psycopg2.connect(DATABASE_URL)
