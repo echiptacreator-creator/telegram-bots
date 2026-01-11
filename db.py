172aa5971581ca8e6502d3ae2ebe32be5355d9d7
Maladesbola <echiptacreator@gmail.com>
2026-01-11 19:07:41 +0500

    Delete db.py

--- db.py
+++
@@ -1,7 +0,0 @@
-import os
-import psycopg2
-
-DATABASE_URL = os.getenv("postgresql://postgres:FMGmkhnZAhcDMkFfLYVBwsGBcrLGEffF@postgres.railway.internal:5432/railway")
-
-def get_conn():
-    return psycopg2.connect(DATABASE_URL)
