7aa554fc39fd6797eb53e586e9b9a80b77fe6f7b
Maladesbola <echiptacreator@gmail.com>
2026-01-11 23:08:28 +0500

    Update avtobot.py

--- avtobot.py
+++ avtobot.py
@@ -26,6 +26,7 @@ from subscription_db import get_all_subs
 from config import PRICE
 from database import get_db
 from database import init_db
+init_db()
 
 car_states = defaultdict(dict)
 
@@ -1154,6 +1155,7 @@ async def save_car(cb: CallbackQuery):
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
+
 
 
 
