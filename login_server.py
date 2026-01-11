e846e5511bf276d32931aec8ac717dbdba1117b6
Maladesbola <echiptacreator@gmail.com>
2026-01-11 19:33:56 +0500

    Update login_server.py

--- login_server.py
+++ login_server.py
@@ -190,11 +190,12 @@ def notify_bot_sync(user_id):
 def run():
     import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 9000))
-    app.run(host="0.0.0.0", port=port)
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 9000))
+        app.run(host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
     run()
+
 
